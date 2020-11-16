4b381a84a548e07351cd1bbc0ce66d050791b603
lbs0912 <lbs1203940926@163.com>
2020-11-17 00:01:13 +0800

    init status

---
+++ src/test/java/com/lbs/spring/my_app/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbs.spring.my_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
