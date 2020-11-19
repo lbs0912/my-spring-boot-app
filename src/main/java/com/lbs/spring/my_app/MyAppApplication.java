e73a8e1eb1d6130bf6e07a1496c2204c5fe8fc08
lbs0912 <lbs1203940926@163.com>
2020-11-20 00:19:34 +0800

    添加MyBatis-Plus 使用Demo

--- src/main/java/com/lbs/spring/my_app/MyAppApplication.java
+++ src/main/java/com/lbs/spring/my_app/MyAppApplication.java
@@ -1,9 +1,11 @@
 package com.lbs.spring.my_app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lbs.spring.my_app.dao")
 public class MyAppApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/lbs/spring/my_app/MyAppApplicationTests.java
+++ src/test/java/com/lbs/spring/my_app/MyAppApplicationTests.java
@@ -1,13 +1,28 @@
 package com.lbs.spring.my_app;
 
+import com.lbs.spring.my_app.dao.UserMapper;
+import com.lbs.spring.my_app.entity.User;
+
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import org.junit.Assert;
+import javax.annotation.Resource;
 
 @SpringBootTest
 class MyAppApplicationTests {
 
+    @Resource
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
     }
 
 }
